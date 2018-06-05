7a438169651a23d02dfc81e4201265f2b526f369
Agustín Clemente <cagustinclemente@gmail.com>
2018-06-05 18:54:55 -0300

    Actualizada la función
    Ahora toma como parámetros dos ID en vez del par de latitudes y longitudes.

--- Grafo/distanciaRutas.py
+++ Grafo/distanciaRutas.py
@@ -1,15 +1,35 @@
 """geo_distance Es el script que me permite calcular las distancias entre dos aeropuertos,
-utilizando la libreria math de Python para realizar determinadas operaciones matermaticas."""
+utilizando la libreria math de Python para realizar determinadas operaciones matermaticas
+y la libreria csv para abrir la lista de aeropuertos."""
 
 from math import cos,radians,sin,pow,asin,sqrt
+import csv
 
-def distancia(lat1, long1, lat2, long2):
-	"""	
+def distancia(id1, id2):
+	"""
 		-Funcionamiento: Esta funcion se encarga de calcular la distancia entre dos aeropuertos
-		-Precondiciones: Recibe como parametros las latitudes y longitudes de los aeropuertos
+		-Precondiciones: Recibe como parametros dos ID de aeropuertos
 		-Postcondiciones: Calcula con ciertas operaciones matematicas la distancia y la devuelve
 	"""
 	radio = 6371 # Este es el radio de la Tierra
+
+	ruta_archivo = "C:\\Users\\Tino\\Desktop\\Facultad\\Algoritmos y estructuras de datos 2\\PROYECTO FINAL\\Datos\\AeropuertosArg.csv"
+	with open(ruta_archivo) as archivo_csv:
+		leer = csv.reader(archivo_csv)
+		encontro = False
+		encontro2 = False
+		for linea in leer:
+			if int(linea[0]) == id1:
+				lat1 = float(linea[4])
+				long1 = float(linea[5])
+				encontro = True
+			if int(linea[0]) == id2:
+				lat2 = float(linea[4])
+				long2 = float(linea[5])
+				encontro2 = True
+		#Si los ID no existen
+		if not encontro or not encontro2:
+			return -1
 
 	lat1 = radians(lat1)
 	lat2 = radians(lat2)
@@ -22,3 +42,6 @@ def distancia(lat1, long1, lat2, long2):
 	a = pow(sin(dlat/2),2) + cos(lat1)*cos(lat2)*pow(sin(dlon/2),2)
 	dist = 2 * radio * asin(sqrt(a))
 	return dist
+	
+# # Testing
+#print(distancia(8,20))
