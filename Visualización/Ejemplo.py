f23262e3ac34a174614dde998a5879bddb9c28c9
Gonzalo Grisafi <gonzagrisa@gmail.com>
2018-06-28 17:31:30 -0300

    Update Ejemplo.py

--- Visualización/Ejemplo.py
+++ Visualización/Ejemplo.py
@@ -1,8 +1,8 @@
 import sys
-sys.path.append("..")
-from Visualización.CrearMapa import CrearMapa
+sys.path.append("..")   #Para localizarse en una carpeta más arriba que la actual
+from Visualización.CrearMapa import CrearMapa     
 from Visualización.AbrirArchivo import AbrirArchivo
 
-CrearMapa()
+CrearMapa() #Crea el archivo html en la carpeta Visualización
 
-AbrirArchivo()
+AbrirArchivo() #Busca el archivo y lo abre en el navegador
