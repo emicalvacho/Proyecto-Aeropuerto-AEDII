e30cfeec8e7f38d1fbb32de0ccc4d189a69e87bf
Agustín Clemente <cagustinclemente@gmail.com>
2018-06-28 12:02:34 -0300

    Modificado para usarse con menu

--- OperacionesGrafos/RecorridoBFS.py
+++ OperacionesGrafos/RecorridoBFS.py
@@ -1,12 +1,10 @@
 import os
 import sys
-sys.path.append("..")
-from Grafo.crearDicGrafo import Graph
 
 def BFS(origen,grafo):
 	visitados = []
 	cola = []
-	print("\nLista de recorrido en anchura\n")
+	print("\nLista de recorrido en anchura a partir del nodo",origen,"\n")
 	cola.append(origen)
 	while cola:
 		actual = cola.pop(0)
@@ -14,14 +12,16 @@ def BFS(origen,grafo):
 			print(actual,"-> ",end="")
 			visitados.append(actual)
 		S = str(actual)
-		for key in grafo[S]:
+		for key in grafo[S]: #grafo.vertices() ?
 			if key not in visitados:
 				cola.append(key)
 
 	print()
 
+"""
 dicc = Graph()
 dicc.crearGrafoCero()
 grafo = dicc.getGrafo()
-BFS(origen,grafo)
+BFS(4,grafo)
 os.system("pause")
+"""
