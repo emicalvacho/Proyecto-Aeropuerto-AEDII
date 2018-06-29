41fecda3c192fe34d20ad6cef797cdeebda4758a
Emiliano Calvacho <39678652+Sir-Calvin@users.noreply.github.com>
2018-06-29 00:55:53 -0300

    Update complejidades.py

--- Eficiencias/complejidades.py
+++ Eficiencias/complejidades.py
@@ -6,11 +6,9 @@ def complejidadesAlg(opc):
 		print("Recorrido DFS es O(V+E) ")
 	if opc == 3:
 		print("Algoritmo DIJKSTRA es O((E+V) * log V)")
-	if opc == 4:
-		print("Algoritmo DIJKSTRA es O((E+V) * log V)")
 	if opc == 5:
-		print("Algoritmo DIJKSTRA es O(V^3)")
+		print("Algoritmo FLOYD-WARSHALL es O(V^3)")
 	if opc == 6:
-		print("Algoritmo DIJKSTRA es O(V)")
+		print("Algoritmo DETECTAR CICLOS es O(V)")
 
 # complejidadesAlg(1)
