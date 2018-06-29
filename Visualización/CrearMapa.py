44d3f0fabf4d70c6f21c48b53a6c17ca08dde2eb
Gonzalo Grisafi <gonzagrisa@gmail.com>
2018-06-29 15:30:59 -0300

    Mejorada la Creación del Mapa

--- Visualización/CrearMapa.py
+++ Visualización/CrearMapa.py
@@ -2,18 +2,33 @@ import folium
 import pandas as pd
 import branca
 import sys
+import random
 import os
 from pathlib import Path
 sys.path.append("..")
 from BuscadorPath.buscarArchivo import buscarArchivo
 
+def colorRandom():
+    return "#"+str(hex(random.randint(0,16777216))[2:])
+
 def CrearMapa():
     ruta=buscarArchivo("AeropuertosArg.csv")
+    ruta2=buscarArchivo("RutasAeropuertos.csv")
     map_osm=folium.Map(location=[-40.1872152,-64.6290514],zoom_start=4)
     #df=pd.read_csv('Argentina.csv',names=['AirportID','Name','City','Country','Latitud','Longitud'])
     df=pd.read_csv(ruta,names=['ID','Nombre','Ciudad','Provincia','Latitud','Longitud'])
+    df2=pd.read_csv(ruta2,names=['ID Origen','ID Destino','Distancia'])
+
+    lista=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+    aislados=dict.fromkeys(lista)
+
+    for index,row in df2.iterrows():
+        aislados[row['ID Origen']]=1
+        aislados[row['ID Destino']]=1
+
+    grupo2=folium.FeatureGroup(name='Aeropuertos Aislados')
+
     for index,row in df.iterrows():
-        #info = "<h4>%s</h4><p>Provincia: %s</p><p>Ciudad: %s</p>" %(row['Nombre'],row['Provincia'],row['Ciudad'])
         info="""
             <p><font face="verdana" color="black">
                 <p style="text-align: center;"><strong>%s</strong></p>
@@ -23,10 +38,42 @@ def CrearMapa():
             </font></p>
         """%(row['Nombre'],row['Provincia'],row['Ciudad'])
         iframe=branca.element.IFrame(html=info,width="400",height="150")
-        folium.Marker(
-        [row['Latitud'],row['Longitud']],
-        popup=folium.Popup(iframe,max_width=400),
-        icon=None).add_to(map_osm)
+        if aislados[row['ID']]==1:
+            folium.Marker(
+            [row['Latitud'],row['Longitud']],
+            popup=folium.Popup(iframe,max_width=400),
+            icon=folium.Icon(color='blue',icon='plane',prefix='fa')).add_to(map_osm)
+        else:
+            folium.Marker(
+            [row['Latitud'],row['Longitud']],
+            popup=folium.Popup(iframe,max_width=400),
+            icon=folium.Icon(color='red',icon='plane',prefix='fa')).add_to(grupo2)
+
+    grupo2.add_to(map_osm)
+
+    latitudes={}
+    longitudes={}
+    ciudades={}
+    for index,row in df.iterrows():
+        latitudes[row['ID']]=round(row['Latitud'],6)
+        longitudes[row['ID']]=round(row['Longitud'],6)
+        ciudades[row['ID']]=str(row['Ciudad'])
+
+    for index,row in df2.iterrows():
+        lineas=[]
+        li=[]
+        li.append(latitudes[row['ID Origen']])
+        li.append(longitudes[row['ID Origen']])
+        lineas.append(li)
+        li=[]
+        li.append(latitudes[row['ID Destino']])
+        li.append(longitudes[row['ID Destino']])
+        lineas.append(li)
+        mostrar=ciudades[row['ID Origen']]+" - "+ciudades[row['ID Destino']]
+        #folium.PolyLine(locations=lineas,color=colorRandom(),weight=3,opacity=1,popup=mostrar).add_to(map_osm)
+        folium.PolyLine(locations=lineas,color=colorRandom(),weight=3.5,opacity=1,popup=mostrar+" "+str(row['Distancia'])+" km").add_to(folium.FeatureGroup(name=mostrar).add_to(map_osm))
+
+    folium.LayerControl().add_to(map_osm)
 
     directorio=Path(os.getcwd())
     directorio=directorio.parent
