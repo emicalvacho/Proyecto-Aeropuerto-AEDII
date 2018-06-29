469be500861d414bfb2a90ad4bebdc7085a38e28
Gonzalo Grisafi <gonzagrisa@gmail.com>
2018-06-29 16:58:03 -0300

    Ahora posee dos funciones 1 para rutas y 1 para aeropuertos

--- Visualización/AbrirArchivo.py
+++ Visualización/AbrirArchivo.py
@@ -4,11 +4,18 @@ import sys
 sys.path.append("..")
 from BuscadorPath.buscarArchivo import buscarArchivo
 
-def AbrirArchivo():
-    archivo='file:///'+buscarArchivo('Mapa.html')
+def AbrirMapaRutas():
+    archivo='file:///'+buscarArchivo('MapaRutas.html')
     browser_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
     try:
         wb.get(browser_path).open_new(archivo)
     except wb.Error:
         wb.open_new(archivo)
-#AbrirArchivo()
+
+def AbrirMapaAeropuertos():
+    archivo='file:///'+buscarArchivo('MapaAeropuertos.html')
+    browser_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+    try:
+        wb.get(browser_path).open_new(archivo)
+    except wb.Error:
+        wb.open_new(archivo)
